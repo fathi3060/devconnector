41488ce11343bf320bfe4dabd0a64709bcfab698
fathi3060 <fathi3060@gmail.com>
2020-07-12 21:06:03 +0200

    ajout profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('Route Profile'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route GET api/profile/me
+// @desc Obtenir le profile de l'utilisateur courant
+// @access Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+        ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({msg: "Il n'y a pas de profile pour cette utilisateur."});
+        }
+
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send("Erreur Serveur !!!");
+    }
+});
 
 module.exports = router;
