777ab119beb26aa76d63e2f0517cf0df89f071bb
fathi3060 <fathi3060@gmail.com>
2020-09-14 19:49:07 +0200

    gestion des commentaires

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,10 @@ const Navbar = ({ auth: {  isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
-                <Link to='/profiles'>
-                    Développeurs
-                </Link>
+                <Link to='/profiles'>Développeurs</Link>
+            </li>
+            <li>
+                <Link to='/posts'>Posts</Link>
             </li>
             <li>
                 <Link to='/dashboard'>
