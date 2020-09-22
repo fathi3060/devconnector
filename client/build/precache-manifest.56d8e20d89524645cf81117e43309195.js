024939f5ced926b9e6d780160c212a4fd302a40e
fathi3060 <fathi3060@gmail.com>
2020-09-22 14:42:37 +0200

    maj

---
+++ client/build/precache-manifest.56d8e20d89524645cf81117e43309195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa82122bb083f9b5da3be8f6d951fdf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f22c839e526ea82ca858",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "2da3196fb3d3c9ce2709",
+    "url": "/static/js/2.64e1eb5a.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.64e1eb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22c839e526ea82ca858",
+    "url": "/static/js/main.ac431751.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
